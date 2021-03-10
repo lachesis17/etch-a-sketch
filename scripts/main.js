c619cae0c52f6bde015a052b491a1990077bf583
lachesis17 <guitarist177@gmail.com>
2021-03-10 21:27:20 +0000

    Added mouseover draw and color select

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ let color = document.getElementById('color');
 let cell = document.createElement('div');
 let gridSize = document.getElementById('gridSlider');
 let reset = document.getElementById('button3');
-
+let griddles = document.querySelectorAll('.grid');
 
 function makeCells(rows, cols) {
   removeGrid(container);
@@ -12,7 +12,15 @@ function makeCells(rows, cols) {
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid";
+    cell.onmouseover = function() {
+      cell.style.backgroundColor = color.value;
+      cell.style.transition = "0.15s ease-in-out";
+      cell.addEventListener('mousedown', function() {
+          cell.style.backgroundColor = "white";
+          cell.style.transition = "0.15s ease-in-out";
+      })
   }
+}
 }
 
 function removeGrid(element) {
@@ -33,5 +41,3 @@ function removeGrid(element) {
   gridSize.addEventListener('input', function() {
       makeCells(this.value, this.value)
   });
-
-  
