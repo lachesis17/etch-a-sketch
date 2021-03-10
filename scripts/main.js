85e28f4e479da7883ee7ef5f2fcee0eb6aa9dc6c
lachesis17 <guitarist177@gmail.com>
2021-03-10 17:25:13 +0000

    Added responsive grid with slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,37 @@
-const container = document.getElementById('container');
+let container = document.getElementById('gridContainer');
+let color = document.getElementById('color');
+let cell = document.createElement('div');
+let gridSize = document.getElementById('gridSlider');
+let reset = document.getElementById('button3');
 
+
+function makeCells(rows, cols) {
+  removeGrid(container);
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (i = 0; i < (rows * cols); i++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid";
+  }
+}
+
+function removeGrid(element) {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  }
+  
+  window.addEventListener('load', (event) => {
+      makeCells(16,16);
+    });
+  
+  reset.onclick = () => {
+      makeCells(16,16);
+      gridSize.value = 16;
+  }
+  
+  gridSize.addEventListener('input', function() {
+      makeCells(this.value, this.value)
+  });
+
+  
