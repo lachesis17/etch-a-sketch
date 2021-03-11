bf20e5f5302e100e66a6b9f3b03a57508b583338
lachesis17 <guitarist177@gmail.com>
2021-03-11 20:46:25 +0000

    Added RGB mode, improved color selector, added Clear button

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 let container = document.getElementById('gridContainer');
-let color = document.getElementById('color');
+let clear = document.getElementById('button1');
 let cell = document.createElement('div');
 let gridSize = document.getElementById('gridSlider');
 let reset = document.getElementById('button3');
-let griddles = document.querySelectorAll('.grid');
 let sliderLabel = document.getElementById('sliderLab');
 let random = document.getElementById('button2');
+let color = document.getElementById('color');
+const rgbToggle = document.getElementById('rgbToggle');
+let rgb = false;
 
 function makeCells(rows, cols) {
   removeGrid(container);
@@ -13,22 +15,22 @@ function makeCells(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid";
-    sliderLabel.innerHTML =  `${rows} x ${cols}`; 
-    cell.onmouseover = function() {
-      cell.style.background = color.value;
-      cell.style.transition = "0.15s ease-in-out";
-        cell.ontouchmove = function() {
-          cell.style.background = color.value;
-          cell.style.transition = "0.15s ease-in-out";
-            cell.addEventListener('mousedown', function() {
-              cell.style.background = "white";
-              cell.style.transition = "0.15s ease-in-out";
-      })
+    container.appendChild(cell);
+    cell.setAttribute("class", "grid");
+    sliderLabel.innerHTML = `${rows} x ${cols}`; 
+      cell.addEventListener("mouseover", function() {
+        if (rgb === true) {
+          rbgMode();
+              cell.style.backgroundColor = `${rgbColors}`;
+              cell.style.transition = "0.15s ease-in";
+      }
+      else {
+              cell.style.backgroundColor = color.value;
+              cell.style.transition = "0.15s ease-in";
+      }
+      });
+    }  
   }
-}
-}
-}
 
 function removeGrid(element) {
     while (element.firstChild) {
@@ -38,6 +40,10 @@ function removeGrid(element) {
   
   window.addEventListener('load', (event) => {
       makeCells(16,16);
+    });
+
+    color.addEventListener('change', function() {
+      this.value = color.value;
     });
   
   reset.onclick = () => {
@@ -49,7 +55,25 @@ function removeGrid(element) {
       makeCells(this.value, this.value)
   });
 
-random.onclick = function () {
+  clear.onclick = function () {
+    let size = gridSize.value;
+  makeCells(size, size);
+  }
+
+  random.onclick = function () {
   color.value = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
   cell.style.backgroundColor = color.value;
-}
+  }
+
+  rgbToggle.addEventListener('change', function() {
+  if (this.checked) {
+    rgb = true;
+  }
+  else {
+    rgb = false;
+    }
+  });
+
+  function rbgMode() {
+    rgbColors = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');;
+  }
